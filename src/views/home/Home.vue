c78e47cf477b7fc2a98c54263aa35603ce0a0f3a
binggang.niu <binggang.niu@tadu.com>
2020-04-21 20:03:40 +0800

    feat：项目代码净化

---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <HelloWorld/>
+    </div>
+</template>
+<script>
+import HelloWorld from '../../components/HelloWorld'
+export default {
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
